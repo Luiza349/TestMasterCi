175d2091adc9a7841a3d0ace8d3a7272a7e6de10
Shafikova Luiza <yalu59@yandex.ru>
2026-09-24 18:01:18 +0300

    test2

--- src/main/java/ru/netology/maventest/MaxCalcService.java
+++ src/main/java/ru/netology/maventest/MaxCalcService.java
@@ -1,4 +1,11 @@
 package ru.netology.maventest;
 
 public class MaxCalcService {
+    public int maxNumber(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
 }
--- src/test/java/ru/netology/maventest/MaxCalcServiceTest.java
+++ src/test/java/ru/netology/maventest/MaxCalcServiceTest.java
@@ -1,4 +1,29 @@
 package ru.netology.maventest;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MaxCalcServiceTest {
+    @Test
+
+    public void maxNumberA() {
+        MaxCalcService myService = new MaxCalcService();
+        int a = 5;
+        int b = 4;
+
+        int expected = a;
+        int actual = myService.maxNumber(a, b);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxNumberB() {
+        MaxCalcService myService = new MaxCalcService();
+        int a = 6;
+        int b = 7;
+
+        int expected = b;
+        int actual = myService.maxNumber(a, b);
+        Assertions.assertEquals(expected, actual);
+    }
 }
